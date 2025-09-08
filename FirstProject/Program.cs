c89d756a838413cb41fd2abad62cc749ebc9fe0d
Hoang Duc Huy <huy25948@gmail.com>
2025-09-09 00:09:11 +0700

    Add files via upload

---
+++ FirstProject/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập số n: ");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Output: " + sum);
+        }
+    }
+}
