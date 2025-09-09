61ba1e58dbc91a10b633986e3c22bdf9bd768084
Hoang Duc Huy <huy25948@gmail.com>
2025-09-09 19:13:27 +0700

    Add files via upload

---
+++ NhapCacPhanTuCuaMang/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+namespace Bai_Tap_ChuyenDe_Csharp
+{
+    class BaiTapCsharp
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int[] arr = new int[10];
+            int i;
+            Console.WriteLine("Đọc và in các phần tử của mảng trong C#");
+            Console.WriteLine("---------------------------------------\n");
+            Console.WriteLine("Nhập 10 phần tử của mảng:");
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Phần tử thứ - {0}: ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("In ra các phần tử của mảng:");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+            Console.WriteLine("\n");
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
